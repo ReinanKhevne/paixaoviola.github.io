34154467529b0ae9f0da7f4686d6d2a3781c94ac
Reinan Khevne <reinanbomfin@gmail.com>
2022-02-22 11:34:18 -0300

    update

---
+++ bnt-mobile.js
@@ -0,0 +1,7 @@
+var bntMobile = document.querySelector('#bnt-mobile')
+bntMobile.addEventListener('click', toggleMenu)
+
+    function toggleMenu() {
+        var nav = document.querySelector('.nav')
+        nav.classList.toggle('active')
+    }
